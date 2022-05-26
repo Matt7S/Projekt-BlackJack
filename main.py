8036215fcef51d0adbb12c536a0ba9d202df8d85
TznPasztet <stokarz@student.agh.edu.pl>
2022-05-26 20:57:45 +0200

    funkcje do uzupełnienia

--- main.py
+++ main.py
@@ -2,15 +2,37 @@ import random
 import os
 import time
 
+
+
 def card_deck():
     card_value = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     card_type = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
-    number_of_decks = 1;
+    number_of_decks = 1
+
+def random_card(card_value,car_type): #funkcja która losuje kartę,zajebista
+
+def hit(): #dobieranie karty
+
+def sum_of_cards(): #liczy sumę kart
+
+def stats(): #oczywiste
+
+def rules(): #oczywiste
 
 
+def iq_krupier(): #poziom zawansowania krupiera(słabiak, dobry krupier, midas)
+
+def play_again(): #oczywiste
+
+def game(): #tutaj dzieje się gra,połączyć z mainem
 
 def print_hi(name):
     print(name)
+
+
+def main():#tutaj dać przywitanie i wszytkie inne pierdoły, wybór opcji, statystyka
+    game()
+
 
 
 if __name__ == '__main__':
