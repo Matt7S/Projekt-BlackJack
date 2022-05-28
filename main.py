cdaba0a45564ca023049ba40c02764dae865d26d
TznPasztet <stokarz@student.agh.edu.pl>
2022-05-28 18:57:41 +0200

    zrobiłem play again, powinno działać

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def card_deck():
     card_value = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
     card_type = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
     number_of_decks = 1
+    for z in range(n):
+        for x in card_value:
+            element = []
+            for y in card_type:
+                element = [x, y]
+                list.append(element)
+    print(list)
+    return list
 
 def random_card(card_value,car_type): #funkcja która losuje kartę,zajebista
 
@@ -23,7 +31,16 @@ def rules(): #oczywiste
 def iq_krupier(): #poziom zawansowania krupiera(słabiak, dobry krupier, midas)
 
 def play_again(): #oczywiste
-
+    again = raw_input("Do you want to play again? (Y/N) : ").lower()
+    if again == "y":
+        dealer_hand = []
+        player_hand = []
+        card_value =['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        card_type = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
+        game()
+    else:
+        print("Bye")
+        exit()
 def game(): #tutaj dzieje się gra,połączyć z mainem
 
 def print_hi(name):
